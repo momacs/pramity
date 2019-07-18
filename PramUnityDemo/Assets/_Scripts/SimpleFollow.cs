2f69195b36c99ad3f59dcc65422da398724bc9d2
PatHealy <pat.healy@pitt.edu>
2019-07-18 16:20:20 -0400

    As of meeting 7-18-19

---
+++ PramUnityDemo/Assets/_Scripts/SimpleFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleFollow : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent ai;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ai.destination = target.transform.position;
+    }
+}
