97fa39bebd4c3baeb10edfcb0633990e4b52607c
PatHealy <pwh5@pitt.edu>
2019-10-17 11:15:28 -0400

    Rounded rule transition masses to whole number values

--- PramFlask/pramity_rules.py
+++ PramFlask/pramity_rules.py
@@ -1,6 +1,7 @@
 from pram.rule import DiscreteInvMarkovChain, TimeAlways, GoToRule, Rule
 from pram.entity import Site, GroupQry, GroupSplitSpec
 import random
+import math
 
 class SimpleFluProgress(DiscreteInvMarkovChain):
 	def __init__(self, var, tm, home, name='markov-chain', t=TimeAlways(), memo=None):
@@ -27,9 +28,14 @@ class MallMovement(Rule):
 		self.p = p
 
 	def apply(self, pop, group, iter, t):
+		playable_mass = group.get_mass_at(GroupQry(attr={"playable": "yes"}))
+		local_mass = group.get_mass_at(GroupQry()) - playable_mass
+		move_chunk = math.ceil(local_mass*self.p)
+
+		move_p = move_chunk/local_mass
 		return [
-			GroupSplitSpec(p=self.p, rel_set={ Site.AT: self.sites[random.randint(0, len(self.sites)-1)] }),
-			GroupSplitSpec(p=1 - self.p)
+			GroupSplitSpec(p=move_p, rel_set={ Site.AT: self.sites[random.randint(0, len(self.sites)-1)] }),
+			GroupSplitSpec(p=1 - move_p)
 		]
 
 	def is_applicable(self, group, iter, t):
@@ -44,12 +50,15 @@ class MallFlu(Rule):
 
 	def apply(self, pop, group, iter, t):
 		flu_prob = self.p *  (group.get_mass_at(GroupQry(attr={'flu-status': 'i'}))/group.get_mass_at(GroupQry()))
+		flu_mass = round(flu_prob * group.get_mass_at(GroupQry(attr={'flu-status': 's'})))
+		flu_prob = flu_mass/group.get_mass_at(GroupQry(attr={'flu-status': 's'}))
+
 		return [
-			GroupSplitSpec(p=self.p, attr_set={ "flu-status": "i"}),
-			GroupSplitSpec(p=1 - self.p)
+			GroupSplitSpec(p=flu_prob, attr_set={ "flu-status": "i"}),
+			GroupSplitSpec(p=1 - flu_prob)
 		]
 
 	def is_applicable(self, group, iter, t):
-		if group.ga("playable") == "yes":
+		if group.ga("playable") == "yes" or group.ga("flu-status") == "i":
 			return False
 		return super().is_applicable(group, iter, t)
